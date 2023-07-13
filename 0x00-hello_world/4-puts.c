2b6eadc263e0a44024dac967aa6677b9651e3c0c
Mohamed Nasser-allah <med.nasserallah@gmail.com>
2023-07-13 18:52:16 +0100

    Update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - display "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return 0
+	return (0);
 }
