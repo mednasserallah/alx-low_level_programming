798d194d6ff6de4f87615aeffeda047d7d4c8afd
Mohamed Nasser-allah <med.nasserallah@gmail.com>
2023-07-14 16:13:11 +0100

    Task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - display last digit of a rundom number and
+ * check if greater than 5, equals 0 or less than 6
+ * Return: 0
+ */
+int main(void)
+{
+	int n, last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+
+	printf("Last digit of %d is %d ", n, last_digit);
+	if (last_digit > 5)
+	{
+		printf("and is greater than 5");
+	}
+	else if (last_digit == 0)
+	{
+		printf("and is 0");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0");
+	}
+	return (0);
+}
