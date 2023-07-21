8a4f64d1f84a6189d676ab7baa40f43ca958a702
Mohamed Nasser-allah <med.nasserallah@gmail.com>
2023-07-21 14:48:34 +0100

    Tasks 100-101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - check the code
+ *
+ * @n: variables
+ *
+ * Return: Always 0.
+ */
+
+void print_number(int n)
+{
+
+	unsigned int integer = n;
+
+	if (n < 0)
+	{
+		integer = -n;
+		_putchar('-');
+	}
+	else
+	{
+		integer = n;
+	}
+
+	if (integer / 10)
+	{
+		print_number(integer / 10);
+	}
+
+	_putchar((integer % 10) + '0');
+}
